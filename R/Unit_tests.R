043c64339ffb30acf307e4e0217941c4f7ea803c
Ella Brayshaw <mm222eb@leeds.ac.uk>
2023-03-14 17:25:49 +0000

    wrote test for y1_y2_prob

---
+++ R/Unit_tests.R
@@ -0,0 +1,16 @@
+
+source("./R/functions.R")
+
+test_y1_sum <- function(){
+  test_prob_y1()
+}
+
+test1 <- function() {
+  # Code testing some functions in functions.R
+  # ...
+}
+
+# Some more unit test functions
+
+# Now call the test functions to find if they pass or fail:
+test1()
