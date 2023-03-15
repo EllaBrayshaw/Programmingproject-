74b7ade45cae0fe0cb7b586211fbf7c1d1cc6a56
Ella Brayshaw <mm222eb@leeds.ac.uk>
2023-03-15 10:17:45 +0000

    Finshed Unit test

--- R/Unit_tests.R
+++ R/Unit_tests.R
@@ -1,16 +1,19 @@
-
-source("./R/functions.R")
-
-test_y1_sum <- function(){
-  test_prob_y1()
+### Test for that prob_y1_y2 sums to one 
+prob_y1_y2 <- function(y_1, y_2, n1, n2) {
+  
+  dbinom(y_1, n1, 0.5)*dbinom(y_2, n2 - n1, 0.5)
 }
 
-test1 <- function() {
-  # Code testing some functions in functions.R
-  # ...
+test_prob_y1_y2 <- function() {
+  n1 <- 30
+  n2 <- 60
+  ys <- expand.grid(y_1 = 0:n1,
+                    y_2 = 0:(n2 - n1))
+  
+  s <-sum(prob_y1_y2(ys$y_1, ys$y_2, n1, n2))
+  return(all.equal(s, 1))
 }
+test_prob_y1_y2()
 
-# Some more unit test functions
 
-# Now call the test functions to find if they pass or fail:
-test1()
+
